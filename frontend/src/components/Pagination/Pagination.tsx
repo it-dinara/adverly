2624dd6a9d037c4c038315ef991b37e05aba4eec
Dinara Usenakunova <ditiviola@gmail.com>
2025-06-09 20:21:31 +0300

    refactor: extract pagination state selection into a dedicated function

--- frontend/src/components/Pagination/Pagination.tsx
+++ frontend/src/components/Pagination/Pagination.tsx
@@ -3,11 +3,21 @@ import { useAppSelector, useAppDispatch } from "Redux/hooks";
 import { setCurrentPage } from "Redux/slices/itemsListSlice";
 import s from "./Pagination.module.css";
 
+interface ItemsState {
+  currentPage: number;
+  items: unknown[];
+  itemsPerPage: number;
+}
+
+interface RootState {
+  items: ItemsState;
+}
+
+const paginationData = (state: RootState): ItemsState => state.items;
+
 const Pagination: React.FC = () => {
   const dispatch = useAppDispatch();
-  const { currentPage, items, itemsPerPage } = useAppSelector(
-    (state) => state.items
-  );
+  const { currentPage, items, itemsPerPage } = useAppSelector(paginationData);
 
   const totalPages = Math.ceil(items.length / itemsPerPage);
 
