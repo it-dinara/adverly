b317cd1789eb60b10b8c784902596f9075e68f5a
Dinara Usenakunova <ditiviola@gmail.com>
2025-06-14 10:41:48 +0300

    refactor: remove dotenv dependency from root package.json and package-lock.json

--- frontend/src/components/Categories/Auto.tsx
+++ frontend/src/components/Categories/Auto.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import s from "./Categories.module.css";
 import { Categories, Car, FormState } from "Types/form";
 import axios from "axios";
